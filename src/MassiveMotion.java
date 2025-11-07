b64270a9d6f77cce683952d94d2aa4495c497a59
Kameron V <66443784+kamv123@users.noreply.github.com>
2025-11-06 16:13:09 -0800

    Refactor MassiveMotion to use properties file
    Updated MassiveMotion class to read configuration from a properties file, allowing dynamic window size and timer delay. Removed hardcoded values and added comet generation probabilities.

--- src/MassiveMotion.java
+++ src/MassiveMotion.java
@@ -2,68 +2,185 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.util.Properties;
 
+/**
+ * Builds output
+ */
 public class MassiveMotion extends JPanel implements ActionListener {
 
     protected Timer tm;
+    protected int windowWidth = 640;
+    protected int windowHeight = 480;
+    protected List<CelestialBody> celestialObjects;
 
-    // TODO: Consider removing the next two lines (coordinates for two balls)
-    protected int x1, y1;
-    protected int x2, y2;
+    // comet creation
+    private double genXProb = 0.05;
+    private double genYProb = 0.05;
+    private int bodySize = 10;
+    private int bodyVelocity = 3;
 
+    /**
+     * Reads through propfile and creates window
+     * @param propfile
+     */
+    public MassiveMotion(String propfile) {
+        try {
+            Properties prop = new Properties();
+            FileInputStream in = new FileInputStream(propfile);
+            prop.load(in);
+            in.close();
 
-    // public MassiveMotion(String propfile) {
-    public MassiveMotion() {
-        // TODO: insert your code to read from configuration file here.
+            int delay = Integer.parseInt(prop.getProperty("timer_delay", "75"));
+            windowWidth = Integer.parseInt(prop.getProperty("window_size_x", "640"));
+            windowHeight = Integer.parseInt(prop.getProperty("window_size_y", "480"));
 
-        tm = new Timer(75, this); // TODO: Replace the first argument with delay with value from config file.
+            tm = new Timer(delay, this);
 
-        // TODO: Consider removing the next two lines (coordinates) for random starting locations.
-        x1 = 100; y1 = 50;
-        x2 = 200; y2 = 400;
+            // Initialize list
+            String listType = prop.getProperty("list", "arraylist").toLowerCase();
+            switch (listType) {
+                case "arraylist":
+                    celestialObjects = new ArrayList<>();
+                    break;
+                case "single":
+                    celestialObjects = new SinglyLinkedList<>();
+                    break;
+                case "double":
+                    celestialObjects = new DoublyLinkedList<>();
+                    break;
+                case "dummyhead":
+                    celestialObjects = new DummyHeadLinkedList<>();
+                    break;
+                default:
+                    System.out.println("Unknown list type. Using ArrayList by default.");
+                    celestialObjects = new ArrayList<>();
+            }
+
+            // Add red star as first celestial object
+            int starX = Integer.parseInt(prop.getProperty("star_position_x", "320"));
+            int starY = Integer.parseInt(prop.getProperty("star_position_y", "240"));
+            int starSize = Integer.parseInt(prop.getProperty("star_size", "30"));
+            int starVelX = Integer.parseInt(prop.getProperty("star_velocity_x", "0"));
+            int starVelY = Integer.parseInt(prop.getProperty("star_velocity_y", "0"));
+            CelestialBody star = new CelestialBody(starX, starY, starSize, starVelX, starVelY, Color.RED);
+            celestialObjects.add(star);
+
+            // Comet creation
+            genXProb = Double.parseDouble(prop.getProperty("gen_x", "0.05"));
+            genYProb = Double.parseDouble(prop.getProperty("gen_y", "0.05"));
+            bodySize = Integer.parseInt(prop.getProperty("body_size", "10"));
+            bodyVelocity = Integer.parseInt(prop.getProperty("body_velocity", "3"));
+
+        } catch (Exception e) {
+            System.out.println("Error loading properties. Using defaults.");
+            tm = new Timer(75, this);
+            celestialObjects = new ArrayList<>();
+            CelestialBody star = new CelestialBody(320, 240, 30, 0, 0, Color.RED);
+            celestialObjects.add(star);
+        }
     }
 
+    /**
+     * Paints stars & comet
+     * @param g the <code>Graphics</code> object to protect
+     */
     public void paintComponent(Graphics g) {
-        super.paintComponent(g); // Probably best you leave this as is.
+        super.paintComponent(g);
 
-        // TODO: Paint each ball. Here's how to paint two balls, one after the other:
-        g.setColor(Color.BLUE);
-        g.fillOval(x1, y1, 20, 20);
+        for (int i = 0; i < celestialObjects.size(); i++) {
+            CelestialBody body = celestialObjects.get(i);
+            g.setColor(body.getColor());
+            g.fillOval(body.getX(), body.getY(), body.getSize(), body.getSize());
+        }
 
-        g.setColor(Color.RED);
-        g.fillOval(x2, y2, 20, 20);
-
-        // Recommend you leave the next line as is
         tm.start();
     }
 
+    /**
+     * updates actions
+     * @param e the event to be processed
+     */
+    public void actionPerformed(ActionEvent e) {
+        // Move existing celestial bodies and remove off-screen comets
+        for (int i = 0; i < celestialObjects.size(); i++) {
+            CelestialBody body = celestialObjects.get(i);
+            body.move();
 
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        // TODO: Change the location of each ball. Here's an example of them moving across the screen:
-        //       ... but to be clear, you should change this.
-        x1 += 10;
-        x2 -= 15;
-        // These two "if" statements keep the balls on the screen in case they go off one side.
-        if (x1 > 640)
-            x1 = 0;
-        if (x2 < 0)
-            x2 = 640;
+            // Only remove comets keep star
+            if (body.getColor() == Color.BLACK && body.isOffScreen(windowWidth, windowHeight)) {
+                celestialObjects.remove(i);
+                i--;
+            }
+        }
 
-        // Keep this at the end of the function (no matter what you do above):
+        // Generate new comets probabilistically
+        generateNewComets();
+
         repaint();
     }
 
+    private void generateNewComets() {
+        // Generate along top/bottom
+        if (Math.random() < genXProb) {
+            int x;
+            double randomValue = Math.random();
+
+            if(randomValue < 0.5) {
+                x=0;
+            } else {
+                x = windowWidth - bodySize;
+            }
+
+            int y = (int) (Math.random() * windowHeight);
+            int vx = randomVelocity(bodyVelocity);
+            int vy = randomVelocity(bodyVelocity);
+            celestialObjects.add(new CelestialBody(x, y, bodySize, vx, vy, Color.BLACK));
+        }
+
+        // Generate along left/right
+        if (Math.random() < genYProb) {
+            int x = (int) (Math.random() * windowWidth);
+            int y;
+            double randomValue = Math.random();
+            if(randomValue < 0.5) {
+                y = 0;
+            } else {
+                y = windowHeight - bodySize;
+            }
+
+            int vx = randomVelocity(bodyVelocity);
+            int vy = randomVelocity(bodyVelocity);
+            celestialObjects.add(new CelestialBody(x, y, bodySize, vx, vy, Color.BLACK));
+        }
+    }
+
+    private int randomVelocity(int max) {
+        int v = 0;
+        while (v == 0) {
+            v = (int) (Math.random() * (2 * max + 1)) - max;
+        }
+        return v;
+    }
+
+    /** Runs simulation
+     */
     public static void main(String[] args) {
         System.out.println("Massive Motion starting...");
-        // MassiveMotion mm = new MassiveMotion(args[0]);
-        MassiveMotion mm = new MassiveMotion();
+
+        if (args.length < 1) {
+            System.out.println("Please provide property file as argument.");
+            System.exit(1);
+        }
+
+        MassiveMotion mm = new MassiveMotion(args[0]);
 
         JFrame jf = new JFrame();
         jf.setTitle("Massive Motion");
-        jf.setSize(640, 480); // TODO: Replace with the size from configuration!
+        jf.setSize(mm.windowWidth, mm.windowHeight);
         jf.add(mm);
-        jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
     }
 }
